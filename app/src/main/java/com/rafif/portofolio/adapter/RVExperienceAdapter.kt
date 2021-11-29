bad3ad9f132cd54997228a4716edc97f3685064f
rafif <rafifaedrayulianto@gmail.com>
2021-11-29 14:49:58 +0700

    initial commit

---
+++ app/src/main/java/com/rafif/portofolio/adapter/RVExperienceAdapter.kt
@@ -0,0 +1,58 @@
+package com.rafif.portofolio.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rafif.portofolio.R
+import com.rafif.portofolio.data.Portofolio
+import com.rafif.portofolio.databinding.ItemExperienceBinding
+
+class RVExperienceAdapter : RecyclerView.Adapter<RVExperienceAdapter.ExperienceViewHolder>(){
+
+    private val listData = ArrayList<Portofolio>()
+
+    var onItemClicked: ((Portofolio) -> Unit)? = null
+
+    fun setData(data: List<Portofolio>?){
+        if (data == null) return
+        listData.clear()
+        listData.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ExperienceViewHolder = ExperienceViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_experience, parent, false)
+    )
+
+
+
+    override fun onBindViewHolder(holder: ExperienceViewHolder, position: Int) {
+        val listData = listData[position]
+        holder.bind(listData)
+    }
+
+    override fun getItemCount(): Int = listData.size
+
+    inner class ExperienceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val binding = ItemExperienceBinding.bind(itemView)
+
+        fun bind(data: Portofolio){
+            binding.experience = data
+            binding.executePendingBindings()
+        }
+
+        init {
+            binding.root.setOnClickListener{
+                onItemClicked?.invoke(listData[adapterPosition])
+            }
+        }
+    }
+
+
+
+
+}
